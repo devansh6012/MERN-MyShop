5fa5f5f583c9958e9f33108a411de0bf88fcba25
devansh6012 <devansh.g6012@gmail.com>
2022-07-22 09:35:07 +0530

    Admin Product implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import UserListScreen from "./screens/UserListScreen";
 import UserEditScreen from "./screens/UserEditScreen";
 import ProductListScreen from "./screens/ProductListScreen";
+import ProductEditScreen from "./screens/ProductEditScreen";
 
 function App() {
   return (
@@ -32,6 +33,7 @@ function App() {
           <Route path='/admin/userlist' component={UserListScreen}/>
           <Route path='/admin/user/:id/edit' component={UserEditScreen}/>
           <Route path='/admin/productlist' component={ProductListScreen}/>
+          <Route path='/admin/product/:id/edit' component={ProductEditScreen}/>
           <Route path='/' component={HomeScreen} exact/>
         </Container>
       </main>
