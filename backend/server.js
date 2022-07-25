4c7a8e53f6e051d8460305ad65e50643e0a4ea8b
devansh6012 <devansh.g6012@gmail.com>
2022-07-25 15:25:29 +0530

    Final commit

--- backend/server.js
+++ backend/server.js
@@ -16,13 +16,24 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('API is running')
-})
 
 app.use('/api/products',productRoutes)
 app.use('/api/users',userRoutes)
 app.use('/api/orders',orderRoutes)
+
+const __dirname = path.resolve()
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    path.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend','build','index.html')))
+}
+else{
+    
+app.get('/', (req, res) => {
+    res.send('API is running')
+})
+}
 
 // middleware
 app.use(notFound)
